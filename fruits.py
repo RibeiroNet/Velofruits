5225ea61b23c3bcf3154ade363ec61cb3e821ac6
Neto Ribeiro <netorribeiro.2007@gmail.com>
2024-05-14 14:30:04 -0300

    add: imagens

--- fruits.py
+++ fruits.py
@@ -2,13 +2,33 @@
 
 import pygame
 
+
 pygame.init()
 
 #TELA
-tela = pygame.display.set_mode((800,500))
 pygame.display.set_caption("Velofruits")
+tela = pygame.display.set_mode((800,500))
 tela.fill((80,120,200))
+
 
 #IMAGEM DE FUNDO
 FUNDO = pygame.image.load("imagens/fundo.png")
 FUNDO = pygame.transform.scale(FUNDO,(800,500))
+
+#RELÓGIO FPS
+clock = pygame.time.Clock()
+
+#RODANDO O JOGO
+funcionando = True
+while funcionando:
+    #EVENTOS 
+    for evento in pygame.event.get():
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            print("Você clicou!!")
+        if evento.type == pygame.QUIT:
+            rodando = False
+
+    tela.blit(FUNDO,(0,0))
+
+#CRIANDO JOGADOR
+jogador1 = Jogador("imagens/.png",40,30,400,460)
